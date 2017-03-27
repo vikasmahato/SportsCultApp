d5aa43fb162ac143ab1773af84eda6b170d180d1
VikasMahato <VikasMahato@3403852.no-reply.drupal.org>
2017-03-27 15:02:36 +0530

    UI changes

--- app/src/main/java/in/sportscult/sportscultapp/HelpFragment.java
+++ app/src/main/java/in/sportscult/sportscultapp/HelpFragment.java
@@ -1,20 +1,25 @@
 package in.sportscult.sportscultapp;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
-/**
- * Created by Ratan on 7/29/2015.
- */
-public class AboutUsFragment extends Fragment {
+public class HelpFragment  extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.about_us_layout,null);
+        return inflater.inflate(R.layout.content_help,null);
+
     }
+
 }
