17441e03c4c097318935b4d1fb5a156d12f481a1
VikasMahato <VikasMahato@3403852.no-reply.drupal.org>
2017-04-02 00:29:45 +0530

    added fragments to backstack

--- app/src/main/java/in/sportscult/sportscultapp/fragments/AboutSFLFragment.java
+++ app/src/main/java/in/sportscult/sportscultapp/fragments/AboutSFLFragment.java
@@ -19,4 +19,6 @@ public class AboutSFLFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.about_sfl,null);
     }
+
+
 }
