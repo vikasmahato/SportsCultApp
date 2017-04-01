d2356e2037472fc42a2884d11e54099da99e35aa
VikasMahato <VikasMahato@3403852.no-reply.drupal.org>
2017-04-01 17:51:02 +0530

    UI changes

--- app/src/main/java/in/sportscult/sportscultapp/fragments/HelpFragment.java
+++ app/src/main/java/in/sportscult/sportscultapp/fragments/HelpFragment.java
@@ -1,17 +1,12 @@
 package in.sportscult.sportscultapp.fragments;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
